e80bf13bc125b3fcec7fe55954a1c20b32506c74
DinaIvanova <dinaivanova285@gmail.com>
2025-05-03 22:41:51 +0300

    first commit

---
+++ StringProcessor.scala
@@ -0,0 +1,17 @@
+object StringProcessor {
+  def processStrings(strings: List[String]): List[String] = {
+    var result = List[String]()
+    for (str <- strings) {
+      if (str.length > 3) {
+        result = result :+ str.toUpperCase
+      }
+    }
+    result
+  }
+
+  def main(args: Array[String]): Unit = {
+    val strings = List("apple", "cat", "banana", "dog", "elephant")
+    val processedStrings = processStrings(strings)
+    println(s"Processed strings: $processedStrings")
+  }
+}
